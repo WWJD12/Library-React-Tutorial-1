8a8129cafebceb0f97e954445c8f8e3fcb16e50e
WWJD12 <c.vance7777@hotmail.com>
2026-02-26 16:32:39 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Landing from './components/Landing';
+import Nav from './components/Nav';
+import Highlights from './components/Highlights';
+import Highlight from './components/Highlight';
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <Landing  />
+      <Highlights />
+      <Highlight />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Highlights.jsx
@@ -0,0 +1,31 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+import Highlight from "./Highlight";
+
+const Highlights = () => {
+    return ( 
+        <section id="highlights">
+            <div className="container">
+                <div className="row">
+                    <h2 className="section__title">
+                        Why choose <span className="blue">Library</span>?
+                        </h2>
+                    <div className="highlight__wrapper">
+                       <Highlight icon={<FontAwesomeIcon icon="bolt" />} 
+                        title="Easy and Quick" 
+                        para="Get access to thousands of books in seconds" />
+
+                         <Highlight icon={<FontAwesomeIcon icon="book-open" />} 
+                        title="10,000+ Books" 
+                        para="Library has books in all genres" />
+
+                         <Highlight icon={<FontAwesomeIcon icon="tags" />} 
+                        title="Affordable" 
+                        para="Get your favorite books at affordable prices" />
+                        </div>
+                        </div>
+                </div>
+        </section>
+        );
+}
+export default Highlights;
