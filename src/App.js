55dbc2477f9641ffe8db539b39bfb41853e426c1
WWJD12 <c.vance7777@hotmail.com>
2026-03-02 13:05:55 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
 import Nav from './components/Nav';
 import Footer from './components/Footer';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Home from './components/Pages/Home';
 import Books from './components/Pages/Books'; 
 import { books } from './data';
 import Bookinfo from './components/Pages/Bookinfo';
+import Cart from './components/Pages/Cart';
+import { useEffect, useState } from 'react';
 
 
 function App() {
+  const [cart, setCart] = useState([]);
+
+  function addToCart(book) { 
+    setCart([...cart, book])
+  }
+
+  useEffect(() => {
+    console.log(cart)
+  }, [cart]) 
+  
   return (
     <Router>
     <div className="App">
       <Nav />
-      <Route path="/" exact component={Home} />
-      <Route path="/books" exact render={() => <Books books={books} />} />
-      <Route path="/books1" render={() => <Bookinfo books={books} />} />
+      <Routes>
+      <Route path="/" exact element={<Home />} />
+      <Route path="/books" exact element={<Books books={books} />} />
+      <Route path="/books/:id" element={<Bookinfo books={books} addToCart={addToCart} />} />
+      <Route path="/cart" element={<Cart books={books} />} />
+     </Routes>
       <Footer />
      </div>
     </Router>
